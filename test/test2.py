37d5dfa0c6cfaf662c0d5c411b0817856513bacc
wnma3mz <wnma3mz@163.com>
2018-03-09 21:05:26 +0800

    update test2.py print

--- test/test2.py
+++ test/test2.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     article_url = "http://mp.weixin.qq.com/s?__biz=MjM5NDU4ODI0NQ==&mid=2650949647&idx=1&sn=854714295ceee7943fe9426ab10453bf&chksm=bd739b358a041223833057cc3816f9562999e748904f39b166ee2178ce1a565e108fe364b920#rd'"
     test = LoginWeChat(appmsg_token, cookie)
     comments = test.get_comments(article_url)
-    comments = test.get_comments(article_url)
-    read_like_num = test.get_read_like_num(article_url)
-
+    read_num, like_num = test.get_read_like_num(article_url)
+    print("comments:")
+    pprint(comments)
+    print("read_like_num:", read_num, like_num)
