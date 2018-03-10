d5efc2dad54e7944e5fd4ec0f69e844229be6d9a
wnma3mz <wnma3mz@163.com>
2018-03-10 23:20:46 +0800

    update test.py get_query_articles

--- test/test.py
+++ test/test.py
@@ -12,12 +12,16 @@ if __name__ == "__main__":
     cookie = "cookie"
     token = "token"
     nickname = "nickname"
+    query = "query"
     #test = OfficialWeChat(username, password)
     test = OfficialWeChat(cookie=cookie, token=token)
 
     articles_sum = test.totalNums(nickname)
-    artiacle_data = test.get_articles(nickname, begin="10", count="5")
+    artiacle_data = test.get_articles(nickname, begin="0", count="5")
     officical_info = test.get_official_info(nickname)
+
+    articles_data_query = test.get_articles(nickname, query=query, begin="0", count="5")
+    articles_sum_query = test.totalNums(nickname, query=query)
 
     print("articles_sum:", end=" ")
     print(articles_sum)
