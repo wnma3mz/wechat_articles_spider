3d399c6bf24d7a5e4f3399d8b764d5a39b7f915d
wnma3mz <wnma3mz@163.com>
2021-06-02 13:11:07 +0800

    remove join

--- wechatarticles/Url2Html.py
+++ wechatarticles/Url2Html.py
@@ -64,7 +64,7 @@ class Url2Html(object):
         img = response.content
         with open(imgpath, "wb") as f:
             f.write(img)
-        return os.path.join(imgpath, name), img
+        return imgpath, img
 
     def replace_img(self, html):
         """
